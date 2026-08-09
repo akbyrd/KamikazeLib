4478229925ca45e835f63c60750fe2abcfef0c34
Adam Byrd <adam1.byrd@gmail.com>
2026-08-09 11:23:50 -0700

    Combat text improvements

--- CombatText.lua
+++ CombatText.lua
@@ -8,19 +8,21 @@ function CT.Init()
 	CT.playerGUID  = UnitGUID("player")
 	CT.lastSource  = nil
 	CT.elements    = {}
-	CT.iTail       = 1
+	CT.oTail       = 0
 	CT.nActive     = 0
+	CT.duration    = nil
 	CT.cfg         = {
-		iconSize     = 24,
-		iconZoom     = .08,
-		iconAspect   = 1.3,
-		font         = nil,
-		fontDesired  = "Interface\\AddOns\\ElvUI\\Game\\Shared\\Media\\Fonts\\Homespun.ttf",
-		fontFallback = "GameFontNormal",
-		fontSize     = 24,
-		fadeDelay    = 1.0,
-		fadeDuration = 0.35,
-		scrollDist   = 30,
+		iconSize      = 24,
+		iconZoom      = .08,
+		iconAspect    = 1.3,
+		font          = nil,
+		fontDesired   = "Interface\\AddOns\\ElvUI\\Game\\Shared\\Media\\Fonts\\Homespun.ttf",
+		fontFallback  = "GameFontNormal",
+		fontSize      = 24,
+		showDuration  = 0.15,
+		hideDuration  = 0.35,
+		totalDuration = 1.5,
+		scrollDist    = 30,
 	}
 
 	CT.frame = CreateFrame("Frame", "KL_COMBAT_TEXT", UIParent)
@@ -35,15 +37,38 @@ function CT.Init()
 	CT.frame:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
 
 	CT.anchorFrame = CT.frame
-
-	-- TODO: Remove
-	local bg = CT.frame:CreateTexture(nil, "BACKGROUND")
-	bg:SetAllPoints()
-	bg:SetColorTexture(0, 0, 0, 0.5)
+	CT.duration = max(CT.cfg.totalDuration, CT.cfg.showDuration + CT.cfg.hideDuration)
 
 	local tempLabel = CT.frame:CreateFontString(nil, "BACKGROUND", nil)
 	local success = tempLabel:SetFont(CT.cfg.fontDesired, 12)
 	CT.cfg.font = success and CT.cfg.fontDesired or CT.cfg.fontFallback
+end
+
+function CT.OnCommand(args)
+	local values = {
+		["1"]       = true,
+		["true"]    = true,
+		["enable"]  = true,
+
+		["0"]       = false,
+		["false"]   = false,
+		["disable"] = false,
+	}
+
+	local enabled = values[args[2]]
+	if enabled ~= nil then
+		CT.SetEnabled(enabled)
+	end
+end
+
+function CT.SetEnabled(enabled)
+	if enabled then
+		CT.frame:RegisterEvent("DAMAGE_METER_COMBAT_SESSION_UPDATED")
+		SetCVar("floatingCombatTextCombatDamage_v2", 0)
+	else
+		CT.frame:UnregisterEvent("DAMAGE_METER_COMBAT_SESSION_UPDATED")
+		SetCVar("floatingCombatTextCombatDamage_v2", 1)
+	end
 end
 
 function CT.OnEvent(frame, event, ...)
@@ -117,7 +142,7 @@ function CT.CreateElement()
 	local yFactor = 1 / max(1, CT.cfg.iconAspect)
 	local texXMin = 0.5 - (0.5 - CT.cfg.iconZoom) * xFactor
 	local texYMin = 0.5 - (0.5 - CT.cfg.iconZoom) * yFactor
-	local icon   = frame:CreateTexture(nil, "BACKGROUND")
+	local icon    = frame:CreateTexture(nil, "BACKGROUND")
 	icon:SetSize(CT.cfg.iconSize * xFactor, CT.cfg.iconSize * yFactor)
 	icon:SetTexCoord(texXMin, 1 - texXMin, texYMin, 1 - texYMin)
 	icon:SetPoint("RIGHT", label, "LEFT", -6, -.0625 * CT.cfg.fontSize)
@@ -128,17 +153,17 @@ function CT.CreateElement()
 		label    = label,
 		yPosInit = nil,
 		tShow    = nil,
+		tHide    = nil,
 	}
 	return elem
 end
 
-function CT.ShowElement(elem, index, spell)
-	elem.yPosInit = CT.anchorOffset + (index - 1) * (CT.cfg.iconSize + 2)
+function CT.ShowElement(elem, offset, spell)
+	elem.yPosInit = CT.anchorOffset + offset * (CT.cfg.iconSize + 2)
 	elem.tShow    = GetTime()
+	elem.tHide    = elem.tShow + CT.duration
 	elem.icon:SetTexture(C_Spell.GetSpellTexture(spell.spellID))
 	elem.label:SetText(AbbreviateNumbers(spell.totalAmount))
-	elem.frame:SetAlpha(1)
-	elem.frame:ClearAllPoints()
 	elem.frame:SetPoint("BOTTOM", CT.anchorFrame, CT.anchorPoint, 0, elem.yPosInit)
 	elem.frame:Show()
 end
@@ -149,38 +174,50 @@ function CT.HideElement(elem)
 	elem.icon:SetTexture("Interface\\Icons\\Inv_misc_questionmark")
 	elem.label:SetText("Inactive")
 	elem.frame:SetAlpha(1)
+	elem.frame:SetScale(1)
+	elem.frame:ClearAllPoints()
 	elem.frame:Hide()
 end
 
--- TODO: Simplify index handling (position, offset, index?)
+function CT.EaseOutBack(t)
+	local c1 = 5
+	local c3 = c1 + 1
+	return 1 + c3*(t - 1)^3 + c1*(t - 1)^2
+end
+
 -- TODO: Document why we can't use C_CombatText
--- TODO: Improve animations (separate in and out, maybe add scale)
--- TODO: Fade current frames if new frames come in early (bloodlust)
 -- TODO: Incoming damage
 -- TODO: Sort by spellId (if built-in function exists)
 
 function CT.DisplaySource(source)
 	local nSpell = #source.combatSpells
 	local nElem  = #CT.elements
+	local tNow   = GetTime()
+
+	-- Fade active elements
+	for i = 0, CT.nActive - 1 do
+		local oElem = (CT.oTail + i) % nElem
+		local elem  = CT.elements[oElem + 1]
+
+		elem.tHide = min(elem.tHide, tNow + CT.cfg.hideDuration)
+		elem.tShow = elem.tHide - CT.duration
+	end
 
 	-- Create new elements
 	while nElem < CT.nActive + nSpell do
-		local elem = CT.CreateElement()
-		table.insert(CT.elements, CT.iTail, elem)
-		if nElem ~= 0 then
-			CT.iTail = CT.iTail + 1
-		end
+		local elem  = CT.CreateElement()
+		table.insert(CT.elements, CT.oTail + 1, elem)
+		CT.oTail = min(CT.oTail + 1, nElem)
 		nElem = nElem + 1
 	end
 
 	-- Show new active elements
-	for iSpell = 1, nSpell do
-		local spell = source.combatSpells[iSpell]
+	for oSpell = 0, nSpell - 1 do
+		local spell = source.combatSpells[oSpell + 1]
 
-		local iElem = (CT.iTail - 1 + CT.nActive + iSpell - 1) % nElem + 1
-		local elem  = CT.elements[iElem]
-
-		CT.ShowElement(elem, iSpell, spell)
+		local oElem = (CT.oTail + CT.nActive + oSpell) % nElem
+		local elem  = CT.elements[oElem + 1]
+		CT.ShowElement(elem, oSpell, spell)
 	end
 	CT.nActive = CT.nActive + nSpell
 end
@@ -190,12 +227,11 @@ function CT.OnUpdate(frame, elapsed)
 	local tNow  = GetTime()
 
 	-- Hide inactive elements
-	for i = 1, CT.nActive do
-		local elem  = CT.elements[CT.iTail]
+	for i = 0, CT.nActive - 1 do
+		local elem = CT.elements[CT.oTail + 1]
 
-		local tHide = elem.tShow + CT.cfg.fadeDelay + CT.cfg.fadeDuration
-		if tNow >= tHide then
-			CT.iTail   = (CT.iTail - 1 + 1) % nElem + 1
+		if tNow >= elem.tHide then
+			CT.oTail   = (CT.oTail + 1) % nElem
 			CT.nActive = CT.nActive - 1
 			CT.HideElement(elem)
 		else
@@ -204,18 +240,24 @@ function CT.OnUpdate(frame, elapsed)
 	end
 
 	-- Animate active elements
-	for i = 1, CT.nActive do
-		local iElem = (CT.iTail - 1 + i - 1) % nElem + 1
-		local elem  = CT.elements[iElem]
+	for i = 0, CT.nActive - 1 do
+		local oElem = (CT.oTail + i) % nElem
+		local elem  = CT.elements[oElem + 1]
 
-		local t    = Clamp((tNow - elem.tShow - CT.cfg.fadeDelay) / CT.cfg.fadeDuration, 0, 1)
-		local yPos = elem.yPosInit + t * CT.cfg.scrollDist
+		local elapsed = tNow - elem.tShow
+		local showOffset = 0
+		local hideOffset = CT.duration - CT.cfg.hideDuration
+		local tIn        = Clamp((elapsed - showOffset) / CT.cfg.showDuration, 0, 1)
+		local tOut       = Clamp((elapsed - hideOffset) / CT.cfg.hideDuration, 0, 1)
+
+		local yPos = elem.yPosInit + tOut * CT.cfg.scrollDist
 		elem.frame:SetPointsOffset(0, yPos)
-		elem.frame:SetAlpha(1 - t)
+		elem.frame:SetAlpha(tIn * (1 - tOut))
+		elem.frame:SetScale(Lerp(.5, 1, CT.EaseOutBack(tIn)))
 	end
 end
 
-CT.Init()
+--CT.Init()
 
 -- NOTE: Have to clear meters because we can't compare amount with previous (totalAmount is secret)
 -- NOTE: Can't ignore spells (spellID is secret)
